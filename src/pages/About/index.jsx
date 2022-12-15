ededb24daec73babc8faa6d54b23df70251720f8
Lucas Souza <108772316+SouzaLucas0@users.noreply.github.com>
2022-12-15 17:05:08 -0300

    Update index.jsx

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -21,15 +21,18 @@ export default function About() {
 
 
       <p className={styles.paragrafo}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        Formado em Sistemas de Informaçã o
+        e buscando consolidar primeiramente
+        carreira em Desenvolvimento
+        Front-End com especialidade em
+        ReactJS.
       </p>
 
       <p className={styles.paragrafo}>
-        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-      </p>
-
-      <p className={styles.paragrafo}>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
+        Posteriormente pretendo
+        adquirir conhecimentos em Back-End,
+        a fim de trilhar a carreira de
+        desenvolvedor Full Stack.
       </p>
     </PostModel>
   )
